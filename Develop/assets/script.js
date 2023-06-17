ba86a57bff7ad9fa9db48fdf7bd9da19f655ce2b
Max <maxrrice@hotmail.com>
2023-06-17 14:35:08 -0500

    pseudocode

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,4 +1,36 @@
 // Assignment code here
+var lettersLow = "abcdefghijklmnopqrstuvwxyz"
+var lowerCase = lettersLow.split('')
+var lettersUp = lettersLow.toUpperCase();
+var upperCase = lettersUp.split('')
+var numbers = "1234567890"
+var numSplit = numbers.split('')
+var specialChars = "`~!@#$%^&*()-_=+<>,.?/:;)"
+var specialSplit = specialChars.split('')
+var userSelections = []
+
+// How long do you want password to be? (prompt)
+    // Store result in variable
+  // Do you want lower case? (Confirm)
+    // If yes
+      // combine arrays
+    //If no
+      // Do nothing
+  // Do you want upper case? (Confirm)
+    // If yes
+      // merge upperCase array into userSelections array
+    // If no
+      // Do nothing
+  // Do you want numbers? (Confirm)
+    // If yes
+      // merge numbers array into userSelections array
+    //If no
+      // Do nothing
+  // Do you want special characters? (confirm)
+    // If yes
+      // merge specialChars array into userSelections array
+    //If no
+      // Do nothing
 
 
 // Get references to the #generate element

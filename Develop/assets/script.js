a32102e8157d848e17c29815d524c6f1d23e419e
Max <maxrrice@hotmail.com>
2023-06-17 17:23:55 -0500

    prompts for user selections

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,42 +1,60 @@
-// Assignment code here
-var lettersLow = "abcdefghijklmnopqrstuvwxyz";
-var lowerCaseArray = lettersLow.split('');
-var lettersUp = lettersLow.toUpperCase();
-var upperCaseArray = lettersUp.split('');
-var numbers = "1234567890";
-var numArray = numbers.split('');
-var specialChars = "`~!@#$%^&*()-_=+<>,.?/:;)";
-var specialCharArray = specialChars.split('');
+// global variables
+var lowerString = "abcdefghijklmnopqrstuvwxyz"; // string containing all letters
+var lowerCaseArray = lowerString.split(''); // splits string into array
+var upperString = lowerString.toUpperCase(); // converts lower case to upper case
+var upperCaseArray = upperString.split('');
+var numString = "1234567890";
+var numArray = numString.split('');
+var specialCharString = "`~!@#$%^&*()<>{}[]|-_=+,.?/:;";
+var specialCharArray = specialCharString.split('');
 var userSelections = [];  // blank array to be filled with user choices
-var passLength = 8;
+var passLength = 8; // 8 is minimum value
 
+function generatePassword() {
+  passLength = parseInt(prompt("How many characters do you want your password to be? \n(Must be between 8 and 128 characters)"))
 
- function generatePassword () {
+  if (isNaN(passLength) || passLength < 8 || passLength > 128) {
+    alert("Please choose an integer between 8 and 128.");
+    return false;
+  }
+
+  if (confirm("Do you want to include lower case letters?"))
+    userSelections = userSelections.concat(lowerCaseArray);
+
+  if (confirm("Do you want to include upper case letters?"))
+    userSelections = userSelections.concat(upperCaseArray);
+
+  if (confirm("Do you want to include numbers?"))
+    userSelections = userSelections.concat(numArray);
+
+  if (confirm("Do you want to include special characters?"))
+    userSelections = userSelections.concat(specialCharArray);
+}
+
 
 // How long do you want password to be? (prompt)
-// Store result in variable
-  passLength = window.prompt("How many characters do you want your password to be?")
-  // Do you want lower case? (Confirm)
-    // If yes
-      // combine arrays
-    // If no
-      // Do nothing
-  // Do you want upper case? (Confirm)
-    // If yes
-      // merge upperCase array into userSelections array
-    // If no
-      // Do nothing
-  // Do you want numbers? (Confirm)
-    // If yes
-      // merge numbers array into userSelections array
-    // If no
-      // Do nothing
-  // Do you want special characters? (confirm)
-    // If yes
-      // merge specialChars array into userSelections array
-    // If no
-      // Do nothing
-}
+  // Store result in variable
+    // Do you want lower case? (Confirm)
+      // If yes
+        // merge lower case array into userSelections array
+      // If no
+        // Do nothing
+    // Do you want upper case? (Confirm)
+      // If yes
+        // merge upper case array into userSelections array
+      // If no
+        // Do nothing
+    // Do you want numbers? (Confirm)
+      // If yes
+        // merge numbers array into userSelections array
+      // If no
+        // Do nothing
+    // Do you want special characters? (confirm)
+      // If yes
+        // merge special characters array into userSelections array
+      // If no
+        // Do nothing
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
